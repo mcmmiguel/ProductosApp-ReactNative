a3cd7a22c90b7333ea4cd01b314cfec158f8669e
Miguel Cobian <miguelcobian99@gmail.com>
2023-12-08 14:20:51 -0600

    Feat. Navigate to Product Screen

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import { StackScreenProps } from '@react-navigation/stack';
+import { ProductsStackParams } from '../navigation/ProductsNavigator';
 
-export const ProductScreen = () => {
+interface Props extends StackScreenProps<ProductsStackParams, 'ProductScreen'> { }
+
+export const ProductScreen = ({ route, navigation }: Props) => {
+
+    const { id, name = '' } = route.params;
+
+    useEffect(() => {
+        navigation.setOptions({
+            title: name ? name : 'New Product',
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <View>
-            <Text>ProductScreen</Text>
+            <Text>{id} {name}</Text>
         </View>
     );
 };
--- src/screens/ProductsScreen.tsx
+++ src/screens/ProductsScreen.tsx
@@ -1,10 +1,30 @@
-import React, { useContext } from 'react';
+/* eslint-disable react/no-unstable-nested-components */
+import React, { useContext, useEffect } from 'react';
 import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import { ProductsContext } from '../context';
+import { StackScreenProps } from '@react-navigation/stack';
+import { ProductsStackParams } from '../navigation/ProductsNavigator';
 
-export const ProductsScreen = () => {
+interface Props extends StackScreenProps<ProductsStackParams, 'ProductsScreen'> { }
+
+export const ProductsScreen = ({ navigation }: Props) => {
 
     const { products } = useContext(ProductsContext);
+
+    useEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <TouchableOpacity
+                    activeOpacity={0.8}
+                    style={styles.addButton}
+                    onPress={() => navigation.navigate('ProductScreen', {})}
+                >
+                    <Text>Add</Text>
+                </TouchableOpacity>
+            ),
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
         <View style={styles.mainContainer}>
@@ -13,7 +33,14 @@ export const ProductsScreen = () => {
                 keyExtractor={(product) => product._id}
 
                 renderItem={({ item }) => (
-                    <TouchableOpacity activeOpacity={0.8}>
+                    <TouchableOpacity
+                        activeOpacity={0.8}
+                        onPress={
+                            () => navigation.navigate('ProductScreen', {
+                                id: item._id,
+                                name: item.nombre,
+                            })
+                        }>
                         <Text style={styles.productName}>{item.nombre}</Text>
                     </TouchableOpacity>
                 )}
@@ -37,5 +64,8 @@ const styles = StyleSheet.create({
         borderBottomWidth: 2,
         marginVertical: 5,
         borderBottomColor: 'rgba(0, 0, 0, 0.1)',
+    },
+    addButton: {
+        marginRight: 20,
     },
 });
